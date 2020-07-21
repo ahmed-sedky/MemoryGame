6cecb15589b8facb31b27c55d84bd153f4d18bf2
ahmed-sedky <61352734+ahmed-sedky@users.noreply.github.com>
2020-07-21 15:46:20 +0200

    second commit

--- JS/Main.js
+++ JS/Main.js
@@ -1,7 +1,44 @@
-var myName =localStorage.getItem('playerName');
-console.log(myName);
+var countDown =document.getElementById('countDown'),
+    seconds =180;
+
+function timeDown(){
+    'use strict';
+    var 
+        hours= Math.floor(seconds/(60*60)),
+        minutes =Math.floor(seconds/60),
+        remSeconds = Math.floor(seconds % 60);
+    if(minutes >= 60){
+        minutes =minutes -60;
+    }
+    if(remSeconds <10){
+        remSeconds = "0"+remSeconds;
+    }
+    if(minutes <10){
+        minutes = "0"+minutes;
+    }
+    if(hours <10){
+        hours = "0"+hours;
+    }
+    
+    countDown.textContent = hours +":" +minutes +":" +remSeconds;
+    if(seconds > 0){
+        seconds =seconds -1;
+    }else{
+        clearInterval(count);
+        countDown.textContent = "You lose!"
+    }   
+}
+
+var count =setInterval(function(){
+    'use strict';
+    timeDown();
+},1000);
+
 document.getElementById('myBut').onclick =function(){
     document.getElementById('playAudio').play();
+    timeDown();
+    var myName =localStorage.getItem('player');
+    console.log(myName);
     var yourName =prompt("what's your name?");
     
     if(yourName ==null || yourName == ""){
@@ -12,12 +49,20 @@ document.getElementById('myBut').onclick =function(){
     }
     document.querySelector(".controls-button").remove();
     }
-    localStorage.setItem('playerName',document.querySelector('.name span').textContent) ;
+    localStorage.setItem('playerScore',document.querySelector('.tries span').textContent) ;
+
+    
 
 var blockContainer =document.querySelector(".memory-game-blocks"),
     duration = 1000,
     blocks = Array.from(blockContainer.children),
     orderRange = [...Array(blocks.length).keys()]; //Array(10) "by3mle 10 3naser fadden"
+
+    
+
+
+
+
 
     shuffle(orderRange);
     //second method
@@ -42,6 +87,20 @@ var blockContainer =document.querySelector(".memory-game-blocks"),
             
             chickMatchedBlocks(allFlippedBlocks[0],allFlippedBlocks[1])
         }
+        var winingBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-matched'));
+        if(winingBlocks.length == 20){
+            console.log("You Win!");
+            document.getElementById('win').style.display = 'block';
+            document.getElementById('playAudio').pause();
+            document.getElementById('wining').play();
+        }
+        if(countDown.textContent == "You lose!"){
+            console.log("You lose!");
+            document.getElementById('lose').style.display = 'block';
+            document.getElementById('playAudio').pause();
+            document.getElementById('losing').play();
+        }
+        
     }
 
     function stopClicking(){
